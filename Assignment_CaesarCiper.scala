0e0da469ecfc1c1a08e7013915b53a2c4d46a9e6
Minuri427 <65712231+Minuri427@users.noreply.github.com>
2020-07-19 00:43:44 +0800

    Answers for Assignment - Caesar ciper

---
+++ Assignment_CaesarCiper.scala
@@ -0,0 +1,10 @@
+object Caesarciper extends App{
+	val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	val string = "HelloWorld"
+	val Encryption = (c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
+	val Decryption = (c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key+26)%  a.size)
+	val ciper = (algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
+	
+	println("Encryption - "+"  "+ciper(Encryption,string,6,alphabet))
+	println("Decryption - "+"  "+ciper(Decryption,string,6,alphabet))
+}
